1e9b94ae8dd77e22b5cb9c5a61e04876f55901c6
Amanda Bohrer <abohrer@equityeng.com>
2016-01-05 11:42:43 -0500

    Amanda

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
 
 def post_list (request):
-    return render(request, 'blog/post_line.html',{})
+    return render(request, 'blog/post_list.html',{})
 
