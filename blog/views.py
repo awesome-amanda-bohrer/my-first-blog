ea6d4baa6267d9027e06354045e1101ea6e14bba
Amanda Bohrer <abohrer@equityeng.com>
2016-01-12 11:34:37 -0500

    a

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,10 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+def my_first_bootstrap(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/index.html')
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
