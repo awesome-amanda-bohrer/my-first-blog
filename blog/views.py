04b005b2edf2865d0ac097fccb37b8254b89040b
Amanda Bohrer <abohrer@equityeng.com>
2016-01-18 12:05:04 -0500

    new template"

--- blog/views.py
+++ blog/views.py
@@ -12,6 +12,10 @@ def my_first_bootstrap(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/index.html')
 
+def my_second_bootstrap(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog2/index.html')
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
