c6cca4e679fb7dd505386c779ea8cc1895dcc48b
Amanda Bohrer <abohrer@equityeng.com>
2016-01-19 14:47:09 -0500

    a

--- bootstrap/urls.py
+++ bootstrap/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^(?i)equity/amanda/$',   views.my_second_bootstrap,  name='hello' ),
+    url(r'^(?i)equity/amanda/$',            views.my_second_bootstrap,  name='hello'    ),
+    url(r'^(?i)equity/amanda/contacts/$',   views.mycontactview,        name='contact'  ),
 ]
 
--- bootstrap/views.py
+++ bootstrap/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def my_second_bootstrap(request):
     return render(request, 'bootstrap/index.html')
+#
+#
+#
+def mycontactview(request):
+    return render(request, 'boostrap/contact.html')
