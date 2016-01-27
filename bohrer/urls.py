54d1399ace1ea50b984a6588859d693f9a3689e1
Amanda Bohrer <abohrer@equityeng.com>
2016-01-27 08:42:09 -0500

    changes

--- bohrer/urls.py
+++ bohrer/urls.py
@@ -2,6 +2,13 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-         url(r'^(?i)equity/bohrer/$',       views.my_third_bootstrap,   name=  'Hi'),
+         url(r'^(?i)equity/bohrer/$',                  views.my_third_bootstrap,   name=  'Hi'          ),
+         url(r'^(?i)equity/bohrer/charts/$',           views.mychartsview,         name=  'charts'      ),
+         url(r'^(?i)equity/bohrer/blankpage/$',        views.myblankpagesview,     name=  'blankpages'  ),
+         url(r'^(?i)equity/bohrer/elements/$',         views.myelementsview,       name=  'elements'    ),
+         url(r'^(?i)equity/bohrer/grid/$',             views.mygridview,           name=  'grid'        ),
+         url(r'^(?i)equity/bohrer/forms/$',            views.myformsview,          name=  'forms'       ),
+         url(r'^(?i)equity/bohrer/rtl/$',              views.myrtlview,            name=  'rtl'         ),
+         url(r'^(?i)equity/bohrer/tables/$',           views.mytablesview,         name=  'tables'      ),
          ]
 
--- bohrer/views.py
+++ bohrer/views.py
@@ -2,3 +2,38 @@ from django.shortcuts import render
 
 def my_third_bootstrap(request):
     return render(request, 'bohrer/index.html')
+#
+#
+#
+def mychartsview(request):
+    return render(request, 'bohrer/charts.html')
+#
+#
+#
+def myblankpagesview(request):
+    return render(request, 'bohrer/blank-page.html')
+#
+#
+#
+def myelementsview(request):
+    return render(request, 'bohrer/bootstrap-elements.html')
+#
+#
+#
+def mygridview(request):
+    return render(request, 'bohrer/bootstrap-grid.html')
+#
+#
+#
+def myformsview(request):
+    return render(request, 'bohrer/forms.html')
+#
+#
+#
+def myrtlview(request):
+    return render(request, 'bohrer/index-rtl.html')
+#
+#
+#
+def mytablesview(request):
+    return render(request, 'bohrer/tables.html')
