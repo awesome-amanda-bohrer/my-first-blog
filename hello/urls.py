019e819e0980f65ebb8c87ea358333c4e57d92cf
Amanda Bohrer <abohrer@equityeng.com>
2016-02-04 11:48:45 -0500

    a

--- hello/urls.py
+++ hello/urls.py
@@ -2,13 +2,13 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^(?i)equity/hello/$',                   views.my_dashboard,      name='dashboard'    ),
-    url(r'^(?i)equity/hello/template/$',          views.template,          name='template'     ),
-    url(r'^(?i)equity/hello/icons/$',             views.icons,             name='icons'        ),
-    url(r'^(?i)equity/hello/maps/$',              views.maps,              name='maps'         ),
-    url(r'^(?i)equity/hello/notifications/$',     views.notifications,     name='notifications'),
-    url(r'^(?i)equity/hello/table/$',             views.table,             name='table'        ),
-    url(r'^(?i)equity/hello/typography/$',        views.typography,        name='typography'   ),
-    url(r'^(?i)equity/hello/upgrade/$',           views.upgrade,           name='upgrade'      ),
-    url(r'^(?i)equity/hello/user/$',              views.user,              name='user'         ),
+    url(r'^(?i)equity/hello/$',                   views.my_dashboard,         name='dashboard'    ),
+    url(r'^(?i)equity/hello/template/$',          views.my_template,          name='template'     ),
+    url(r'^(?i)equity/hello/icons/$',             views.my_icons,             name='icons'        ),
+    url(r'^(?i)equity/hello/maps/$',              views.my_maps,              name='maps'         ),
+    url(r'^(?i)equity/hello/notifications/$',     views.my_notifications,     name='notifications'),
+    url(r'^(?i)equity/hello/table/$',             views.my_table,             name='table'        ),
+    url(r'^(?i)equity/hello/typography/$',        views.my_typography,        name='typography'   ),
+    url(r'^(?i)equity/hello/upgrade/$',           views.my_upgrade,           name='upgrade'      ),
+    url(r'^(?i)equity/hello/user/$',              views.my_user,              name='user'         ),
     ]
--- hello/views.py
+++ hello/views.py
@@ -5,42 +5,42 @@ def my_dashboard(request):
 #
 #
 #
-def template(request):
+def my_template(request):
     return render(request, 'hello/template.html')
 #
 #
 #
-def icons(request):
+def my_icons(request):
     return render(request, 'hello/icons.html')
 #
 #
 #
-def maps(request):
+def my_maps(request):
     return render(request, 'hello/maps.html')
 #
 #
 #
-def notifications(request):
+def my_notifications(request):
     return render(request, 'hello/notifications.html')
 #
 #
 #
-def table(request):
+def my_table(request):
     return render(request, 'hello/table.html')
 #
 #
 #
-def typography(request):
+def my_typography(request):
     return render(request, 'hello/typography.html')
 #
 #
 #
-def upgrade(request):
+def my_upgrade(request):
     return render(request, 'hello/upgrade.html')
 #
 #
 #
-def user(request):
+def my_user(request):
     return render(request, 'hello/user.html')
 #
 #
