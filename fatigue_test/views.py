7fb56bf425fe3538fee48f450201514a60c257a2
Amanda Bohrer <abohrer@equityeng.com>
2016-03-07 10:38:42 -0500

    a

--- fatigue_test/views.py
+++ fatigue_test/views.py
@@ -1,142 +1,142 @@
 from django.shortcuts import render
 
 def my_dashboard2(request):
-    return render(request, 'fatigue/dashboard.html')
+    return render(request, 'fatigue_test/dashboard.html')
 #
 #
 #
 def my_template2(request):
-    return render(request, 'fatigue/template.html')
+    return render(request, 'fatigue_test/template.html')
 #
 #
 #
 def my_buttons2(request):
-    return render(request, 'fatigue/buttons.html')
+    return render(request, 'fatigue_test/buttons.html')
 #
 #
 #
 def my_calendar2(request):
-    return render(request, 'fatigue/calendar.html')
+    return render(request, 'fatigue_test/calendar.html')
 #
 #
 #
 def my_charts2(request):
-    return render(request, 'fatigue/charts.html')
+    return render(request, 'fatigue_test/charts.html')
 #
 #
 #
 def my_datatable2(request):
-    return render(request, 'fatigue/datatable.html')
+    return render(request, 'fatigue_test/datatable.html')
 #
 #
 #
 def my_elements2(request):
-    return render(request, 'fatigue/elements.html')
+    return render(request, 'fatigue_test/elements.html')
 #
 #
 #
 def my_extended2(request):
-    return render(request, 'fatigue/extended.html')
+    return render(request, 'fatigue_test/extended.html')
 #
 #
 #
 def my_fullscreen2(request):
-    return render(request, 'fatigue/fullscreen.html')
+    return render(request, 'fatigue_test/fullscreen.html')
 #
 #
 #
 def my_google2(request):
-    return render(request, 'fatigue/google.html')
+    return render(request, 'fatigue_test/google.html')
 #
 #
 #
 def my_grid2(request):
-    return render(request, 'fatigue/grid.html')
+    return render(request, 'fatigue_test/grid.html')
 #
 #
 #
 def my_htmltutorial2(request):
-    return render(request, 'fatigue/html-tutorial.html')
+    return render(request, 'fatigue_test/html-tutorial.html')
 #
 #
 #
 def my_icons2(request):
-    return render(request, 'fatigue/icons.html')
+    return render(request, 'fatigue_test/icons.html')
 #
 #
 #
 def my_lock2(request):
-    return render(request, 'fatigue/lock.html')
+    return render(request, 'fatigue_test/lock.html')
 #
 #
 #
 def my_login2(request):
-    return render(request, 'fatigue/login.html')
+    return render(request, 'fatigue_test/login.html')
 #
 #
 #
 def my_notifications2(request):
-    return render(request, 'fatigue/notifications.html')
+    return render(request, 'fatigue_test/notifications.html')
 #
 #
 #
 def my_panels2(request):
-    return render(request, 'fatigue/panels.html')
+    return render(request, 'fatigue_test/panels.html')
 #
 #
 #
 def my_plainpage2(request):
-    return render(request, 'fatigue/plain-page.html')
+    return render(request, 'fatigue_test/plain-page.html')
 #
 #
 #
 def my_register2(request):
-    return render(request, 'fatigue/register.html')
+    return render(request, 'fatigue_test/register.html')
 #
 #
 #
 def my_sweetalert2(request):
-    return render(request, 'fatigue/sweet-alert.html')
+    return render(request, 'fatigue_test/sweet-alert.html')
 #
 #
 #
 def my_regular3(request):
-    return render(request, 'fatigue/regular.html')
+    return render(request, 'fatigue_test/regular.html')
 #
 #
 #
 def my_typography2(request):
-    return render(request, 'fatigue/typography.html')
+    return render(request, 'fatigue_test/typography.html')
 #
 #
 #
 def my_user2(request):
-    return render(request, 'fatigue/user.html')
+    return render(request, 'fatigue_test/user.html')
 #
 #
 #
 def my_validation2(request):
-    return render(request, 'fatigue/validation.html')
+    return render(request, 'fatigue_test/validation.html')
 #
 #
 #
 def my_vector2(request):
-    return render(request, 'fatigue/vector.html')
+    return render(request, 'fatigue_test/vector.html')
 #
 #
 #
 def my_wizard2(request):
-    return render(request, 'fatigue/wizard.html')
+    return render(request, 'fatigue_test/wizard.html')
 #
 #
 #
 def my_regular4(request):
-    return render(request, 'fatigue/regulartable.html')
+    return render(request, 'fatigue_test/regulartable.html')
 #
 #
 #
 def my_extended3(request):
-    return render(request, 'fatigue/extendedtable.html')
+    return render(request, 'fatigue_test/extendedtable.html')
 #
 #
 #
