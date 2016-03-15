70717acbc2e04d8eff66d016d01fa11101b15943
Amanda Bohrer <abohrer@equityeng.com>
2016-03-15 08:41:46 -0400

    a

--- bohrer/static/bohrer/js/selection.js
+++ bohrer/static/bohrer/js/selection.js
@@ -1,5 +1,5 @@
 $(function(){
-	$('div.product-chooser').not('.disabled').find('div.product-chooser-item').on('click', function(){
+	$('div.product-chooser').find('div.product-chooser-item').not('.disabled').on('click', function(){
 		$(this).parent().parent().find('div.product-chooser-item').removeClass('selected');
 		$(this).addClass('selected');
 		$(this).find('input[type="radio"]').prop("checked", true);
