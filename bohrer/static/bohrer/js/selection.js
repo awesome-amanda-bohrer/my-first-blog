984d6caec7e5f0274c11f44b4311b56ad8b14f29
Amanda Bohrer <abohrer@equityeng.com>
2016-03-16 08:53:41 -0400

    a

--- bohrer/static/bohrer/js/selection.js
+++ bohrer/static/bohrer/js/selection.js
@@ -1,12 +1,4 @@
- $(document).ready(function() {
-    $("[rel='tooltip']").tooltip();    
- 
-    $('.thumbnail').hover(
-        function(){
-            $(this).find('.caption').slideDown(250); //.fadeIn(250)
-        },
-        function(){
-            $(this).find('.caption').slideUp(250); //.fadeOut(205)
-        }
-    ); 
- });
+$(document).ready(function() {
+    $('#list').click(function(event){event.preventDefault();$('#products .item').addClass('list-group-item');});
+    $('#grid').click(function(event){event.preventDefault();$('#products .item').removeClass('list-group-item');$('#products .item').addClass('grid-group-item');});
+});
